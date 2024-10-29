3820c145739614dbb663e0ac197c9b832c9d3921
Sid <83157044+siddhartha008@users.noreply.github.com>
2024-10-29 01:41:25 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  'base': "/megha-bhattarai/",
   plugins: [react()],
     build: {
     rollupOptions: {
@@ -12,7 +13,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': 'http://localhost:5000', // Backend server URL
-      'base': "/megha-bhattarai/",
+      
 
     },
   },
