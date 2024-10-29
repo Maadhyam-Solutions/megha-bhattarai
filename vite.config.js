7b6ff4d1a28a5f74f5bcb2811f94588b204a5eab
Siddhartha Mishra <sid@Siddharthas-MacBook-Pro.local>
2024-10-29 01:15:09 -0500

    base change vite.config

--- vite.config.js
+++ vite.config.js
@@ -12,7 +12,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': 'http://localhost:5000', // Backend server URL
-      'base': "/megha-bhattarai",
+      'base': "/megha-bhattarai/",
 
     },
   },
