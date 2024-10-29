6624e5c453f8f40c7c74a4fef435744be4948c4a
Sid <83157044+siddhartha008@users.noreply.github.com>
2024-10-29 01:00:04 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,12 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+    build: {
+    rollupOptions: {
+      external: ['react-icons/fa'], // Add this line if you want to externalize it
+    },
+  },
+
   server: {
     proxy: {
       '/api': 'http://localhost:5000', // Backend server URL
