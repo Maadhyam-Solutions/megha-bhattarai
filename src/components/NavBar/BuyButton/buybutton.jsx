b76d3d075f95870c4b04873f018cd9cedb04ff25
Sid <83157044+siddhartha008@users.noreply.github.com>
2025-06-25 09:25:12 -0500

    Update buybutton.jsx

--- src/components/NavBar/BuyButton/buybutton.jsx
+++ src/components/NavBar/BuyButton/buybutton.jsx
@@ -2,10 +2,12 @@ import React, { useState } from 'react';
 import './buybutton.css'
 
 export default function BuyButton() {
-
+  const handleClick = () => {
+    window.location.href = 'https://docs.google.com/forms/d/e/1FAIpQLSerh9UFtYsiE5ixmDh51ZJ0lNq1ceXednb6AXEXO11ZkGvqNQ/viewform';
+  };
   return (
     <div>
-      <button className='buyMyBookBtn'>Coming Soon</button>
+      <button onClick={handleClick} className='buyMyBookBtn'>Buy Now</button>
     </div>
   );
 }
